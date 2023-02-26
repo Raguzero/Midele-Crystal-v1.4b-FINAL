92ef3ae13dbbd15e5a0b9e9d7371d1362fef1903
Raguzero <14874194+Raguzero@users.noreply.github.com>
2023-02-26 14:07:04 +0100

    Smashing rocks has a chance to contain items
    Credits to: https://github.com/pret/pokecrystal/wiki/Smashing-rocks-has-a-chance-to-contain-items

--- engine/events/checkforhiddenitems.asm
+++ engine/events/checkforhiddenitems.asm
@@ -83,3 +83,35 @@ CheckForHiddenItems: ; b8172
 	inc hl
 	ret
 ; b81ea
+
+RockItemEncounter:
+	ld hl, .RockItems
+	call Random
+.loop
+	sub [hl]
+	jr c, .ok
+	inc hl
+	inc hl
+	jr .loop
+
+.ok
+	ld a, [hli]
+	inc a
+	jr z, .done
+	ld a, [hli]
+.done
+	ld [wScriptVar], a
+	ret
+	
+.RockItems:
+	db 1, MAX_REVIVE
+	db 2, THICK_CLUB
+	db 4, NUGGET
+	db 6, STAR_PIECE
+	db 12, BIG_PEARL
+	db 18, ETHER
+	db 24, HARD_STONE
+	db 24, SOFT_SAND
+	db 48, PEARL
+	db 64, BRICK_PIECE
+	db -1
