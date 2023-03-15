8b4565a22d5dbd1334667898b1bdb2299908cfed
Raguzero <14874194+Raguzero@users.noreply.github.com>
2023-03-15 15:50:56 +0100

    Update badge_cap.asm

--- home/badge_cap.asm
+++ home/badge_cap.asm
@@ -20,20 +20,21 @@ GetBadgeLevel::
 	ret
 
 BadgeLevelTable: ; 3fef
-	db $14 ; 0 badges, level 20
-	db $1e ; 1 badge, level 30
-	db $1e ; 2 badges, level 30
-	db $23 ; 3 badges, level 35
-	db $28 ; 4 badges, level 40
-	db $2d ; 5 badges, level 45
-	db $32 ; 6 badges, level 50
-	db $37 ; 7 badges, level 55
-	db $4b ; 8 badges, level 75
-	db $50 ; 9 badges, level 80
-	db $55 ; 10 badges, level 85
-	db $55 ; 11 badges, level 85
+	db $f ; 0 badges, level 15
+	db $14 ; 1 badge, level 20
+	db $19 ; 2 badges, level 25
+	db $1e ; 3 badges, level 30
+	db $23 ; 4 badges, level 35
+	db $28 ; 5 badges, level 40
+	db $28 ; 6 badges, level 40
+	db $2d ; 7 badges, level 45
+	db $41 ; 8 badges, level 65
+	; añadir level cap activado desde campeon liga, pero como??? level 70
+	db $46 ; 9 badges, level 75
+	db $50 ; 10 badges, level 80
+	db $50 ; 11 badges, level 80
 	db $55 ; 12 badges, level 85
-	db $5a ; 13 badges, level 90
+	db $55 ; 13 badges, level 85
 	db $5a ; 14 badges, level 90
 	db $5a ; 15 badges, level 90
 	db $64 ; 16 badges, level 100
